0a3b0070dfa00af8cc629e6bdc0bdf83cc4f1a90
ItaloSixx <anonimosbang@gmail.com>
2024-10-16 10:06:36 -0300

    home view

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function home(): View
+    {
+        return view('home');
+    }
+
+    public function gereneteExercices()
+    {
+        echo 'gerando';
+    }
+
+    public function printExercices()
+    {
+
+    }
+
+    public function exportExercices()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo 'matX';
+Route::controller(MainController::class)->group(function(){
+    Route::get('/', 'home')->name('home');
+    Route::post('/generate-exercices', 'gereneteExercices')->name('generateExercices');
+    Route::get('/print-exercices', 'printExercices')->name('printExercices');
+    Route::get('/export-exercices', 'export-exercices')->name('exportExercices');
 });
